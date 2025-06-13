e3ee4058c9833aedb6fe43f74310bda3df5e0e9b
Max Gemereth <177759368+gemerethmax@users.noreply.github.com>
2025-06-13 11:30:16 -0400

    get random quote route set up. Front end successfully renders returned quote object.

--- server.js
+++ server.js
@@ -8,4 +8,14 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+app.get('/api/quotes/random', (req, res, next)  => {
+    const randomQuote= getRandomElement(quotes)
+    if (randomQuote) {
+        res.status(200).send({quote: randomQuote});
+    } else {
+        res.status(404).send({ error: 'No quotes found' });
+    }
+})
+
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
